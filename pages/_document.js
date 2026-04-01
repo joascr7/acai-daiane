3e4d6b63a28ef7d711e4923fb755dcd8935668b4
joascr7 <contatojoasvieiral@email.com>
2026-04-01 15:15:06 -0300

    update

--- pages/_document.js
+++ pages/_document.js
@@ -2,12 +2,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
-      />
+    <Html lang="pt-BR">
+      <Head>
+        {/* 🔥 VIEWPORT CORRETO */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
