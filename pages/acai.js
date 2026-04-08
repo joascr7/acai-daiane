255d2e9dcc12903c18d6483e30e6455e4237a71c
joascr7 <contatojoasvieiral@email.com>
2026-04-07 22:10:36 -0300

    update

--- pages/acai.js
+++ pages/acai.js
@@ -3079,7 +3079,7 @@ return (
     </div>
 
     <div style={{
-  height: `calc(${NAVBAR}px + env(safe-area-inset-bottom) + 18px)`,
+  height: `calc(${NAVBAR}px + env(safe-area-inset-bottom) + 48px)`,
   background: "#fff"
 }} />
 
