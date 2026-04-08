8173c18f04531046e6be1e27001bcca8b687bebc
joascr7 <contatojoasvieiral@email.com>
2026-04-08 20:49:50 -0300

    update

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,3 +1,27 @@
-self.addEventListener("push", function (event) {
-  console.log("Push recebido:", event);
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBTR3bccO-PnPFlfxNtel2gfbupup_goLU",
+  authDomain: "acai-ddff1.firebaseapp.com",
+  projectId: "acai-ddff1",
+  storageBucket: "acai-ddff1.appspot.com",
+  messagingSenderId: "218618921752",
+  appId: "1:218618921752:web:c32de26abd5adfbcc4546d"
+});
+
+const messaging = firebase.messaging();
+
+
+// 🔔 RECEBER NOTIFICAÇÃO EM BACKGROUND
+messaging.onBackgroundMessage(function (payload) {
+  console.log("🔔 Mensagem recebida:", payload);
+
+  const notificationTitle = payload.notification?.title || "Novo aviso";
+  const notificationOptions = {
+    body: payload.notification?.body || "",
+    icon: "/icon-192.png"
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
