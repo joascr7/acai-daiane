23bbb3613815e60c116812832e22304c53348239
joascr7 <contatojoasvieiral@email.com>
2026-04-08 20:42:02 -0300

    fix sw simple

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,13 +1,3 @@
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
-
-firebase.initializeApp({
-  apiKey: "AIzaSyBTR3bccO-PnPFlfxNtel2gfbupup_goLU",
-  authDomain: "acai-ddff1.firebaseapp.com",
-  projectId: "acai-ddff1",
-  storageBucket: "acai-ddff1.appspot.com",
-  messagingSenderId: "218618921752",
-  appId: "1:218618921752:web:c32de26abd5adfbcc4546d"
+self.addEventListener("push", function (event) {
+  console.log("Push recebido:", event);
 });
-
-const messaging = firebase.messaging();
