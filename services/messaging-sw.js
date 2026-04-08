52b768f78f9b7a35a0dfc2d339a7e2eb7b944f11
joascr7 <contatojoasvieiral@email.com>
2026-04-08 19:37:23 -0300

    update

---
+++ services/messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBTR3bccO-PnPFlfxNtel2gfbupup_goLU",
+  authDomain: "acai-ddff1.firebaseapp.com",
+  projectId: "acai-ddff1",
+  storageBucket: "acai-ddff1.appspot.com",
+  messagingSenderId: "218618921752",
+  appId: "1:218618921752:web:c32de26abd5adfbcc4546d"
+});
+
+firebase.messaging();
