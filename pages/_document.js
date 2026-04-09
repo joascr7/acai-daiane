815845f3b50e6b840ddcd837192866258b3166e6
joascr7 <contatojoasvieiral@email.com>
2026-04-08 21:18:12 -0300

    update

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,11 @@ export default function Document() {
             <meta name="apple-mobile-web-app-capable" content="yes" />
             <meta name="theme-color" content="#ea1d2c" />
 
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+            <meta name="apple-mobile-web-app-title" content="Açaí Daiane" />
+            <link rel="apple-touch-icon" href="/icon-192.png" />
+
        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </Head>
 
