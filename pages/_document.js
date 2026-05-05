4f5987a2801524eaf08baa5c1a2dd92808f5ce4e
joascr7 <contatojoasvieiral@email.com>
2026-05-05 17:13:24 -0300

    update

--- pages/_document.js
+++ pages/_document.js
@@ -4,35 +4,22 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
-        {/* VIEWPORT */}
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover"
-        />
+        {/* 🔥 VIEWPORT CORRETO */}
+        
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
 
-        {/* PWA / iOS */}
-        <meta name="theme-color" content="#ffffff" />
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="theme-color" content="#ea1d2c" />
 
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+            <meta name="apple-mobile-web-app-title" content="Açaí Daiane" />
+            <link rel="apple-touch-icon" href="/icon-192.png" />
 
-        {/* 🔥 REMOVE TRANSLUCENT (ESSENCIAL) */}
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="default"
-        />
-
-        <meta name="apple-mobile-web-app-title" content="Açaí Daiane" />
-
-        <link rel="apple-touch-icon" href="/icon-192.png" />
+       <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </Head>
 
-      <body
-        style={{
-          margin: 0,
-          padding: 0,
-          background: "#ffffff"
-        }}
-      >
+      <body>
         <Main />
         <NextScript />
       </body>
