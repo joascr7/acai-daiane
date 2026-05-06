3c3c082d55997a9f0818fe583857efc328029bb9
joascr7 <contatojoasvieiral@email.com>
2026-05-06 14:59:51 -0300

    update

--- pages/acai.js
+++ pages/acai.js
@@ -5926,7 +5926,7 @@ return (
       position: "relative",
       boxShadow: "0 8px 24px rgba(0,0,0,0.08)",
       width: "100%",
-      height: 180, // 👈 mobile fixo
+      height: 200, // 👈 mobile fixo
       background: "#111"
     }}
   >
